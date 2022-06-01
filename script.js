c4e4a217a5c5d23a959d671b9813a6b39110e377
Sushant Sharma <79782587+sushantsharma0221@users.noreply.github.com>
2022-06-01 18:05:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+let searchContainer = document.querySelector(".search-container");
+let search = document.querySelector(".search");
+search.addEventListener("click", () => {
+  searchContainer.classList.toggle("active");
+});
