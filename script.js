c883b8022ee19db40187c95f7f7a325f5e507112
Sushant Sharma <79782587+sushantsharma0221@users.noreply.github.com>
2022-06-01 18:04:46 +0530

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-let searchContainer = document.querySelector(".search-container");
-let search = document.querySelector(".search");
-search.addEventListener("click", () => {
-  searchContainer.classList.toggle("active");
-});
